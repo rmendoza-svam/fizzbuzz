05e99da3dcad97db0044500f86b570d99d50ed9e
Rafael Mendoza <rmendoza@svam.com>
2022-04-08 15:35:35 -0400

    unit test

---
+++ Fizz-Buzz/infrastructure/Constants.cs
@@ -0,0 +1,21 @@
+﻿namespace Fizz_Buzz.infrastructure
+{
+    public static class Constants
+    {
+        public const string EmptyValue = "<Empty>";
+        public const string InvalidItem = "Invalid Item";
+        public const string FizzBuzz = "FizzBuzz";
+        public const string Fizz = "Fizz";
+        public const string Buzz = "Buzz";
+
+        public enum DataValue
+        {
+            Empty,
+            Invalid,
+            DividedBy3And5,
+            Fizz,
+            Buzz,
+            FizzBuzz
+        }
+    }
+}
---
+++ TestFizzBuzz/Models/FizzBuzzModelTest.cs
@@ -0,0 +1,55 @@
+﻿using Fizz_Buzz.infrastructure;
+using Fizz_Buzz.Models;
+using Xunit;
+
+namespace FizzBuzz.Tests.Models
+{
+    public class FizzBuzzModelTest
+    {
+        [Fact]
+        public void createModel()
+        {
+            var model = new Fizz_BuzzModel();
+            Assert.True(model.Data != null);
+        }
+        [Fact]
+        public void CalculateThree()
+        {
+            var model = new Fizz_BuzzModel();
+            model.Data[0] = "3";
+            model.Calculate();
+            Assert.True(model.DataResult[0,1] == Constants.Fizz);
+        }
+        [Fact]
+        public void CalculateFive()
+        {
+            var model = new Fizz_BuzzModel();
+            model.Data[0] = "5";
+            model.Calculate();
+            Assert.True(model.DataResult[0, 1] == Constants.Buzz);
+        }
+        [Fact]
+        public void CalculateFiveAndThree()
+        {
+            var model = new Fizz_BuzzModel();
+            model.Data[0] = "30";
+            model.Calculate();
+            Assert.True(model.DataResult[0, 1] == Constants.FizzBuzz);
+        }
+        [Fact]
+        public void CalculateEmptyValue()
+        {
+            var model = new Fizz_BuzzModel();
+            model.Calculate();
+            Assert.True(model.DataResult[0, 0] == Constants.EmptyValue);
+        }
+        [Fact]
+        public void CalculateInvalidItem()
+        {
+            var model = new Fizz_BuzzModel();
+            model.Data[0] = "A30A";
+            model.Calculate();
+            Assert.True(model.DataResult[0, 1] == Constants.InvalidItem);
+        }
+    }
+}
